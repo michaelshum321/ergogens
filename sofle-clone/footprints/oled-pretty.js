26148b62d795728df7cc43b89c1429ec80933d84
Michael Shum <michael.shum@newfront.com>
2023-10-12 20:25:17 -0700

    Add OLED kicad footprint, front and back Add ergogen footprint for OLED

---
+++ sofle-clone/footprints/oled-pretty.js
@@ -0,0 +1,42 @@
+module.exports = {
+    params: {
+        designator: 'OLED',
+        side: 'F',
+        VCC: {type: 'net', value: 'VCC'},
+        GND: {type: 'net', value: 'GND'},
+        SDA: undefined,
+        SCL: undefined
+    },
+    body: p => {
+        const fn = (pos, neg) => `
+            (module OLED (layer ${p.side}.Cu) (tedit 5E1ADAC2)
+            ${p.at /* parametric position */}
+
+            ${'' /* footprint reference */}
+            (fp_text reference "${p.ref}" (at 0 -0.24) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1 1) (thickness 0.15))))
+            (fp_text value OLED (at 0 -3.81) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
+
+            ${'' /* Fab outline */}
+            (fp_line (start ${neg}6 1.5) (end ${neg}6 -36.5) (layer ${p.side}.Fab) (width 0.12))
+            (fp_line (start ${neg}6 -36.5) (end ${pos}6 -36.5) (layer ${p.side}.Fab) (width 0.12))
+            (fp_line (start ${pos}6 1.5) (end ${pos}6 -36.5) (layer ${p.side}.Fab) (width 0.12))
+            (fp_line (start ${pos}6 1.5) (end ${neg}6 1.5) (layer ${p.side}.Fab) (width 0.12))
+
+            ${'' /* Silkscreen line on top of headers */}
+            (fp_line (start -5 -1.27) (end 5 -1.27) (layer ${p.side}.SilkS) (width 0.25))
+
+            ${'' /* Header pins */}
+            (pad SDA thru_hole circle (at ${neg}3.81 0 ${p.rot}) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask))
+            (pad SCL thru_hole circle (at ${neg}1.27 0 ${p.rot}) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask))
+            (pad VCC thru_hole circle (at ${pos}1.27 0 ${p.rot}) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask))
+            (pad GND thru_hole roundrect (at ${pos}3.81 0 ${p.rot}) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask) (roundrect_rratio 0.25))
+            )
+        `;
+
+        if (side === 'F') {
+            return fn('', '-');
+        } else if (side === 'B') {
+            return fn('-', '');
+        }
+    }
+}
